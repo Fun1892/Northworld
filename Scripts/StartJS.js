cbcc840f4f67a50a5a88d9c12441b92379648bd7
Fun1892 <112317945+Fun1892@users.noreply.github.com>
2023-04-02 20:47:57 +0200

    Delete StartJS.js

--- Scripts/StartJS.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById("glow-btn");
-
-btn.addEventListener("click", () => {
-btn.classList.add("glow");
-setTimeout(() => {
-btn.classList.remove("glow");
-}, 200);
-});
